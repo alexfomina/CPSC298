7c8600498d563d7c1f8642381e6d8078fc9398cb
alexfomina <121912437+alexfomina@users.noreply.github.com>
2023-01-05 17:09:26 -0800

    Add files via upload

---
+++ Project1_HelloCpp/hello_namespace.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hello_namespace
+//
+//  Created by Alexandra Fomina on 1/5/23.
+//
+// @file hello_namespace.cpp
+// @brief Outputs Hello C++! and uses a namespace
+// @author Alexandra Fomina fomina@chapman.edu
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // insert code here...
+    cout << "Hello C++!" << endl;
+    return 0;
+}
