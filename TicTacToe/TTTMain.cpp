72bbc3f6de51cb8ac0d3cad8c68173fdc12bbdf5
alexfomina <121912437+alexfomina@users.noreply.github.com>
2023-01-21 15:26:08 -0800

    Add files via upload

---
+++ TicTacToe/TTTMain.cpp
@@ -0,0 +1,14 @@
+#include "TTT.h"
+
+
+
+int main() {
+
+    Game game;
+    std::cout << "Tic Tac Toe" << std::endl;
+    std::cout << "The top row is row number 0, the middle row number 1, and the bottom row is row number 2." << std::endl;
+    std::cout << "The left column is column number 0,, the middle column number 1, and the right column is column number 1." << std::endl;
+    std::cout << std::endl;
+    game.play();
+
+}
